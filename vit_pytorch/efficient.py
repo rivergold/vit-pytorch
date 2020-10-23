f3f69a23a20a4ed9c223b4f211d74bb1016e282e
Jing He <hejing01@qiyi.com>
2020-10-23 14:06:00 +0800

    Format python files

--- vit_pytorch/efficient.py
+++ vit_pytorch/efficient.py
@@ -2,12 +2,20 @@ import torch
 from einops import rearrange
 from torch import nn
 
+
 class ViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, transformer, channels = 3):
+    def __init__(self,
+                 *,
+                 image_size,
+                 patch_size,
+                 num_classes,
+                 dim,
+                 transformer,
+                 channels=3):
         super().__init__()
         assert image_size % patch_size == 0, 'image dimensions must be divisible by the patch size'
-        num_patches = (image_size // patch_size) ** 2
-        patch_dim = channels * patch_size ** 2
+        num_patches = (image_size // patch_size)**2
+        patch_dim = channels * patch_size**2
 
         self.patch_size = patch_size
 
@@ -18,17 +26,17 @@ class ViT(nn.Module):
 
         self.to_cls_token = nn.Identity()
 
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, dim * 4),
-            nn.GELU(),
-            nn.Linear(dim * 4, num_classes)
-        )
+        self.mlp_head = nn.Sequential(nn.LayerNorm(dim),
+                                      nn.Linear(dim, dim * 4), nn.GELU(),
+                                      nn.Linear(dim * 4, num_classes))
 
     def forward(self, img):
         p = self.patch_size
 
-        x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p, p2 = p)
+        x = rearrange(img,
+                      'b c (h p1) (w p2) -> b (h w) (p1 p2 c)',
+                      p1=p,
+                      p2=p)
         x = self.patch_to_embedding(x)
 
         cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
